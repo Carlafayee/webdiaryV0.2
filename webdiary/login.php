efcfca9abf21020248350740a395a260aaae424b
ching <39197883+zenraiching@users.noreply.github.com>
2021-03-27 21:04:51 +0800

    bagong dagdag

--- webdiary/login.php
+++ webdiary/login.php
@@ -6,38 +6,30 @@ include("connect.php");
 
 $msg = ""; 
 if(isset($_POST['save'])) {
-  $username = trim($_POST['username']);
-  $password = trim($_POST['password']);
-  if($username != "" && $password != "") {
-    try {
-      $query = "select * from `login_user` where `user_name`=:username and `password`=:password";
-      $stmt = $con->prepare($query);
-      $stmt->bindParam('username', $username, PDO::PARAM_STR);
-      $stmt->bindValue('password', $password, PDO::PARAM_STR);
-      $stmt->execute();
-      $count = $stmt->rowCount();
-      $row   = $stmt->fetch(PDO::FETCH_ASSOC);
-      if($count == 1 && !empty($row)) {
-        /******************** Your code ***********************/
-        $_SESSION['sess_user_id']   = $row['uid'];
-        $_SESSION['sess_user_name'] = $row['username'];
-        $_SESSION['sess_name'] = $row['name'];
-       header("location:home.php");
+  $sql = "SELECT * FROM `users` WHERE User_Name=:Username and Password=:Password";
 
-      } else {
-        $msg = "Invalid username and password!";
-      }
-    } catch (PDOException $e) {
-      echo "Error : ".$e->getMessage();
-    }
-  } else {
-    $msg = "Both fields are required!";
+  $statement=$con->prepare($sql);
+
+  $statement->bindParam(":Username",$_POST["username"]);
+  $statement->bindParam(":Password", $_POST["password"]);
+  $result = $statement->execute();
+  
+  
+  if($result){
+    $row = $statement->fetchAll();
+    
+    $_SESSION['users']= $row[0]["User_Name"];
+    $_SESSION['full_name']= $row[0]["First_Name"] . $row[0]["Last_Name"];
+    
   }
+
+
+
 }
 
-if(isset($_SESSION['sess_user_id'])) {
+if(isset($_SESSION['users'])) {
         header("Location:home.php");
-    }
+}
 ?>
 
 <!doctype html>
